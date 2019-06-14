74d779c3e3850f6137c005dc0687dec50d02e897
baixiaosh <782179350@qq.com>
2019-06-14 15:15:46 +0800

    更新

--- 2019-06-14/demo/src/components/HelloWorld.vue
+++ 2019-06-14/demo/src/components/HelloWorld.vue
@@ -1,28 +1,44 @@
 <template>
-	<div class="hello">
-		<Loading/>
-		<Test v-if="visible">这个是ID等于1</Test>
-		<Test v-else>这个是ID等于2</Test>
-	</div>
+  <div class="hello">
+    <Loading/>
+    <div v-if="visible" key="332">
+      <span>text1111:</span>
+      <input key="222" type="text">
+      <input type="text">
+    </div>
+    <div v-else key="2121">
+      <span>text22222:</span>
+      <input type="text">
+      <input key="2333" type="text">
+    </div>
+    <div v-if="visible" key="1">
+      <Test data-id="1" >这个是ID等于1</Test>
+    </div>
+    <div v-else key="2">
+      <Test data-id="2" >这个是ID等于2</Test>
+    </div>
+  
+  </div>
 </template>
 
 <script>
-import Test from './Test'
+import Test from "./Test";
 export default {
-	name: "HelloWorld",
-	components: {
-		Test
-	},
-	data() {
-		return {
-			visible: true
-		}
-	},
-	mounted() {
-		setInterval(() => {
-			this.visible = !this.visible
-		}, 4000);
-	},
+  name: "HelloWorld",
+  components: {
+    Test
+  },
+  data() {
+    return {
+      inputVal: "",
+      visible: true
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.visible = !this.visible;
+    }, 4000);
+  }
 };
 </script>
 
