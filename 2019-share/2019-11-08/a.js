09bee9d431b5df711603cac2f1e943810b925739
heyu <664475324@qq.com>
2019-11-15 16:54:26 +0800

    2019-11-08 Signed-off-by: heyu <664475324@qq.com>

---
+++ 2019-share/2019-11-08/a.js
@@ -0,0 +1,4 @@
+for(let i = 0; i < 10000 ; i++){
+    console.log(i);
+}
+console.log("a",new Date().valueOf());
---
+++ 2019-share/2019-11-08/b.js
@@ -0,0 +1,2 @@
+console.log("b",new Date().valueOf());
+alert("这里我们可以看到图片资源进行加载了一部分，然后再加载的底部脚本文件并执行!");
